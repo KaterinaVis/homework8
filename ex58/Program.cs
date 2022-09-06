93e305d6230531c5a86d748b70620a7e1174d8d7
KaterinaVis <katerina4484@mail.ru>
2022-09-06 20:05:25 +0500

    ex58

--- ex58/Program.cs
+++ ex58/Program.cs
@@ -17,3 +17,59 @@
 // 20 81 8 6
 // 56 8 4 24
 // 10 6 24 49
+
+Console.WriteLine("Введите число строк массива m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] matrixA = new int [m,n];
+int [,] matrixB = new int [m,n];
+int [,] matrixMultip = new int [m,n];
+
+FillRndArray(matrixA);
+FillRndArray(matrixB);
+PrintArray (matrixA);
+Console.WriteLine();
+PrintArray (matrixB);
+Console.WriteLine();
+MultipArray(matrixA, matrixB, matrixMultip);
+Console.Write($"Произведение матриц будет равно следующему массиву:");
+Console.WriteLine();
+PrintArray (matrixMultip);
+
+void PrintArray (int[,] matr) //печать
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+Console.Write($"{matr[i, j]} ");
+}
+Console.WriteLine();
+}
+}
+
+void FillRndArray(int[,] matr) //заполнение
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+Random rand = new Random();
+matr[i, j] = rand.Next(1, 10);
+}
+}
+}
+
+void MultipArray(int[,] matrA, int[,] matrB, int[,] matrC)
+{
+for (int i = 0; i < matrC.GetLength(0); i++)
+{
+for (int j = 0; j < matrC.GetLength(1); j++)
+{
+matrC[i, j] = matrA[i,j]*matrB[i,j];
+}
+}
+}
